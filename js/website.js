77e04e5ab17ec6d77a3c9f61cc30fc745f10dcb5
Prem Bonsall <pbonsall@Development.HealthyChoices.gsi.gov.uk>
2018-10-16 16:47:17 +0100

    :moneybag: Added donate button and js

--- js/website.js
+++ js/website.js
@@ -5,4 +5,29 @@ function openModal() {
   function closeModal() {
     document.getElementById('myModal').style.display = "none";
   }
+
   
+function openSnackbar() {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+   // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+
+  //get text to show
+  document.getElementById("snackbar").innerHTML = getText();
+}
+
+function getText(){
+  var textArray = [
+    "Why...",
+    "I appreciate the thought",
+    "Don't be silly",
+    "Questionable choice"
+  ]
+  var phrase = textArray[Math.floor(Math.random()*textArray.length)];
+  return phrase
+}
