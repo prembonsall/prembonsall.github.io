c622df62bcaeaf88c19871203a7e01c933cce2ba
Prem Bonsall <pbonsall@Development.HealthyChoices.gsi.gov.uk>
2018-10-17 13:36:37 +0100

    :scroll: Added comments and seperate files for css

--- js/website.js
+++ js/website.js
@@ -8,16 +8,13 @@ function openModal() {
 
   
 function openSnackbar() {
-  // Get the snackbar DIV
+  // Get the css for the pop-up
   var x = document.getElementById("snackbar");
-
-   // Add the "show" class to DIV
   x.className = "show";
+  // Hide after duration
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 2500);
 
-  // After 3 seconds, remove the show class from DIV
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-
-  //get text to show
+  //Get text to show
   document.getElementById("snackbar").innerHTML = getText();
 }
 
